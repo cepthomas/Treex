6f93331d9bf02f0d5a10ac30094de108b5eb97c4
cepthomas <cepthomas@gmail.com>
2025-06-19 17:43:05 -0500

    Works.

--- App.cs
+++ App.cs
@@ -16,10 +16,8 @@ namespace Treex
         readonly HashSet<string> excludeDirectories = [];
         readonly Dictionary<string, ConsoleColorEx> extColors = [];
 
-        // TODO useful? readonly string logFileName;
-
         // Visuals. Unicode is default.
-        readonly record struct Visuals (string Tee, string Ell, string Vert, string Hor);
+        readonly record struct Visuals (string Tee, string Ell, string Pipe, string Empty);
         readonly Visuals Unicode = new("├── ", "└── ", "│   ", "    ");
         readonly Visuals Ascii = new("+---", @"\---", "|   ", "    ");
         readonly Visuals vis;
@@ -40,10 +38,11 @@ namespace Treex
         /// <summary>Build me one.</summary>
         public App(string[] args)
         {
+            int code = 0;
+
             try
             {
                 string appDir = MiscUtils.GetAppDataDir("Treex", "Ephemera");
-                //logFileName = Path.Combine(appDir, "log.txt");
 
                 ///// Init runtime values from default ini file. TODO or new one from cmd line?
                 var inrdr = new IniReader(Path.Join(appDir, "treex_default.ini"));
@@ -162,20 +161,20 @@ namespace Treex
             catch (IniSyntaxException ex)
             {
                 Console.WriteLine($"Syntax error({ex.LineNum}): {ex.Message}");
-                Environment.Exit(1);
+                code = 1;
             }
             catch (ArgumentException ex)
             {
                 Console.WriteLine($"{ex.Message}");
-                Environment.Exit(1);
+                code = 2;
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"{ex.GetType()}: {ex.Message}");
-                Environment.Exit(1);
+                code = 3;
             }
 
-            Environment.Exit(0);
+            Environment.Exit(code);
         }
 
         /// <summary>
@@ -190,55 +189,50 @@ namespace Treex
 
             var di = new DirectoryInfo(startDir);
 
-            // Print files first.
-            if (showFiles)
+            ///// Collect contents first.
+            var files = showFiles ? di.GetFiles().ToList() : [];
+            files.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+            var dirs = di.GetDirectories().Where(d => !excludeDirectories.Contains(d.Name)).ToList();
+            dirs.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+            ///// Print files.
+            foreach (var file in files)
             {
-                var files = di.GetFiles().ToList();
-                files.Sort((x, y) => x.Name.CompareTo(y.Name));
+                var fn = file.FullName; // full path
+                var nm = file.Name;
+                var last = file == files.Last();
 
-                foreach (var file in files)
+                if (dirs.Count > 0)
                 {
-                    var fn = file.FullName; // full path
-                    var nm = file.Name;
-                    var last = file == files.Last();
-
-                    if (file == files.Last())
-                    {
-                        Print($"{prefix}{vis.Ell}");
-                        PrintValue(file);
-                    }
-                    else
-                    {
-                        Print($"{prefix}{vis.Hor}");
-                        PrintValue(file);
-                    }
+                    Print($"{prefix}{vis.Pipe}");
+                    PrintValue(file);
+                }
+                else
+                {
+                    Print($"{prefix}{vis.Empty}");
+                    PrintValue(file);
                 }
             }
 
-            // Then dirs.
-            var dirs = di.GetDirectories().ToList();
-            dirs.Sort((x, y) => x.Name.CompareTo(y.Name));
-
+            ///// Then dirs.
             foreach (var dir in dirs)
             {
                 var fn = dir.FullName; // full path
                 var nm = dir.Name;
 
-                if (!excludeDirectories.Contains(nm))
+                if (dir == dirs.Last())
                 {
-                    if (dir == dirs.Last())
-                    {
-                        Print($"{prefix}{vis.Hor}");
-                        PrintValue(dir);
-                        PrintTree(fn, $"{prefix}{vis.Hor}", depth + 1); // => recurse
+                    Print($"{prefix}{vis.Ell}");
+                    PrintValue(dir);
+                    PrintTree(fn, $"{prefix}{vis.Empty}", depth + 1); // => recurse
 
-                    }
-                    else
-                    {
-                        Print($"{prefix}{vis.Tee}");
-                        PrintValue(dir);
-                        PrintTree(fn, $"{prefix}{vis.Vert}", depth + 1); // => recurse
-                    }
+                }
+                else
+                {
+                    Print($"{prefix}{vis.Tee}");
+                    PrintValue(dir);
+                    PrintTree(fn, $"{prefix}{vis.Pipe}", depth + 1); // => recurse
                 }
             }
         }
@@ -255,9 +249,9 @@ namespace Treex
                 var bytes = new FileInfo(fi.FullName).Length;
                 slen = bytes switch
                 {
-                    >= 1024 * 1024 => $" ({bytes / 1024 / 1024}M)",
-                    >= 1024 => $" ({bytes / 1024}K)",
-                    _ => $" ({bytes}B)"
+                    >= 1024 * 1024 => $" ({bytes / 1024 / 1024}m)",
+                    >= 1024 => $" ({bytes / 1024}k)",
+                    _ => $" ({bytes}b)"
                 };
             }
 
@@ -305,147 +299,4 @@ namespace Treex
             Console.WriteLine("    -? help");
         }
     }
-
-    /*
-var infos = di.GetFileSystemInfos().ToList();
-infos.Sort((x, y) => x.Name.CompareTo(y.Name));
-foreach (var fsi in infos)
-{
-    //var ext = fsi.Extension;
-    var fn = fsi.FullName; // full path
-    var nm = fsi.Name;
-    var isDir = IsDirectory(fsi);
-    var last = fsi == infos.Last();
-
-    var exclude = (isDir && excludeDirectories.Contains(nm)) || (!isDir && !showFiles);
-
-    switch (exclude, isDir, last)
-    {
-       case (true, _, _):
-           continue;
-
-       case (false, true, false): // dir
-            Print($"{prefix}{vis.Tee}");
-            PrintValue(fsi);
-            PrintTree(fn, $"{prefix}{vis.Vert}", depth + 1); // => recurse
-            break;
-
-       case (false, true, true): // last dir
-            Print($"{prefix}{vis.Hor}");
-            PrintValue(fsi);
-            PrintTree(fn, $"{prefix}{vis.Hor}", depth + 1); // => recurse
-            break;
-
-        case (false, false, false): // file
-            Print($"{prefix}{vis.Hor}");
-            PrintValue(fsi);
-            break;
-
-        case (false, false, true): // last file
-            Print($"{prefix}{vis.Ell}");
-            PrintValue(fsi);
-            break;
-    }
-}
-*/
-
-    /*
-/// <summary>
-/// Print one file system entry. Selects color.
-/// </summary>
-/// <param name="fsi"></param>
-void PrintValue(FileSystemInfo fsi)
-{
-    ConsoleColorEx clr;
-    var slen = "";
-
-    if (IsDirectory(fsi))
-    {
-       clr = dirColor;
-    }
-    else
-    {
-        if (showSize)
-        {
-            var bytes = new FileInfo(fsi.FullName).Length;
-            slen = bytes switch
-            {
-                >= 1024 * 1024 => $" ({bytes / 1024 / 1024}M)",
-                >= 1024 => $" ({bytes / 1024}K)",
-                _ => $" ({bytes}B)"
-            };
-        }
-
-        string ext = fsi.Extension.ToLower();
-        clr = extColors.TryGetValue(ext, out ConsoleColorEx clrex) ? clrex : fileColor;
-    }
-
-    Print($"{fsi.Name}{slen}", clr);
-    Print(Environment.NewLine);
-}
-*/
-
-
-
-    // void WriteColored(string text, ConsoleColor color)
-    // {
-    //     SetColor(color);
-    //     Write(text);
-    //     SetColor(_defaultColor);
-    // }
-
-    // ConsoleColor GetColor(FileSystemInfo fsItem)
-    // {
-    //     if (IsDirectory(fsItem))
-    //     {
-    //        return _dirColor;
-    //     }
-
-    //     string ext = Path.GetExtension(fsItem.FullName).ToLower();
-    //     if (ExeExtensions.Contains(ext))
-    //     {
-    //        return _exeColor;
-    //     }
-
-    //     return _fileColor;
-    // }
-
-
-    //[Serializable]
-    //public class UserSettings : SettingsCore
-    //{
-    //    [DisplayName("File Names")]
-    //    [Description("Include file names - default")]
-    //    // [Category("NTerm")]
-    //    [Browsable(true)]
-    //    public bool IncludeFiles { get; set; } = true;
-
-    //    [DisplayName("Show Size")]
-    //    [Description("Show size - default")]
-    //    [Browsable(true)]
-    //    public bool ShowSize { get; set; } = true;
-
-    //    [DisplayName("Iteration Depth")]
-    //    [Description("How deep to look - default")]
-    //    [Browsable(true)]
-    //    public int MaxDepth { get; set; } = 3;
-
-    //    [DisplayName("Ascii Characters")]
-    //    [Description("Use ascii charactrs else unicode")]
-    //    [Browsable(true)]
-    //    public bool AsciiChars { get; set; } = true;
-
-    //    // Show unix "hidden" files/dirs like .git, .vs, .gitignore
-    //    [DisplayName("Include Hidden")]
-    //    [Description("Include hidden files (.*)")]
-    //    [Browsable(true)]
-    //    public bool IncludeHidden { get; set; } = false;
-
-    //    //// like bin, obj, ...
-    //    //[DisplayName("Ignore Directories")]
-    //    //[Description("Ignore directories - base")]
-    //    //[Category("NTerm")]
-    //    //[Browsable(true)]
-    //    //public List<string> IgnoreDirectories { get; set; } = [];
-    //}
 }
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Treex
 Fancy tree command for Windows.
+
+## Configuration
+
+Copy `treex_default.ini` to `%APPDATA%\Ephemera\Treex\treex_default.ini` and edit to taste.
+
+Defaults can be overidden on the command line:
+
+`treex [dir] [-f] [-d N] [-s] [-?] [-i fld 1,fld2,...] [-u fld1,fld2,...]`
+
+Option              | Description                       | Default Override
+-----------         | -----------                       | ------------------------
+dir                 | start folder or '.' if missing    | NA
+-d num              | maxDepth (0 means all)            | Y
+-f                  | show files                        | Y
+-s                  | show size (file only)             | Y
+-i fld1,fld2,...    | ignore folders  ()                | adds to default
+-u fld1,fld2,...    | unignore folders  ()              | removes from default
+-?                  | help                              |
+
